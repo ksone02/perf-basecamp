f7f9a582bfdaa0ec272ddd2b88025adf13228c8f
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-09-05 10:51:48 +0900

    chore: ghpages 배포를 위한 build

--- src/models/help/artist.ts
+++
@@ -1,7 +0,0 @@
-export const Artist = ({ name = '', profileUrl = '', profileImageUrl = '' }) => ({
-  name,
-  profileUrl,
-  profileImageUrl
-});
-
-export interface Artist extends ReturnType<typeof Artist> {}
